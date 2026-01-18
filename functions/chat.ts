4e7414c7aefe96d80da20fb92c02ce568151ada0
Gxyenn 正式 <GxyennDEV@gmail.com>
2026-01-18 19:20:18 +0700

    Upload 1 item(s) via Uploader Pro

--- functions/chat.ts
+++ functions/chat.ts
@@ -67,7 +67,7 @@ Aroganzz-DarkTrasherNet Siap Dengan Perintah Anda
 `;
 
     const res = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=" +
         env.GEMINI_API_KEY,
       {
         method: "POST",
